bd7a0644712414142d1f98e92e3a6d0c179dfc1b
Phee-Phi-Pho-Phum <hanchi1578@gmail.com>
2026-01-25 16:52:37 +0800

    Update 1
    I tweaked the code, there is multiple functions this time, however this version does not currently showcase any output value, that still needs to be fixed.

--- Rock, Paper scissors.py
+++ Rock, Paper scissors.py
@@ -1,5 +1,5 @@
 
-'''Rock paper scisors'''
+'''Rock paper scissors'''
 
 #input value: rock paper or scissors
 #output will be rock paper or scissors
@@ -7,50 +7,54 @@
 #if computer outputs scissors, user win,
 # if computer outputs paper, computer wins
 
-#Note any option that says (s c i s s Ors), bassically if it spells out scissors it's
-# okay wether its captial or not, likewise with any other option 
-
-# x = input("Pick an option, Rock, Paper or Scissors :")
-# x = str(x)
-# if x != "rock" or "scissors" or "rock":
-#     print("There are no options like that")
-# else:
-#     print("Yes")
-
-a = input("what would you like to play, rock paper of scissors.: " )
-
-# def import_random():
-#     import random
-#     x = ["0","1"]
-#     random.shuffle(x)
-#     return x
+def user_input():
     
-def calc():
-    import random
-    x = ["0","1"]
-    random.shuffle(x)
-    
-    x_ = ["0","1"]
-    if x_ == x:
-        # Hard mode
-        if a == "rock":
-            print("paper")
-            
-        elif a == "paper":
-            print("scissors")
-            
-        else:
-            print("rock")
-            
+    u_in = input("Pick one option, rock, paper or scissors: " )
+    u_in =''.join(u_in.split()).lower()
+    if  u_in == "rock" or u_in == "paper" or  u_in =="scissors":
+        return u_in
     else:
-        # Easy mode ["1","0"]
-        if a == "rock":
-            print("scissors")
-             
-        elif a == "paper":
-            print("rock")
+        return f"Input value {u_in} is not specified as an option in the program,\nPlease try again."
         
+def gen_random():
+    import random
+    return random.choice(["0","1"])
+    
+def calc1(u_in,gen_random):
+    
+    
+    if gen_random == ["0"]:
+        # Ai wins
+        if u_in == "rock":
+            a = "paper"
+            return a
+        elif u_in == "paper":
+            b = "scissors"
+            return b
         else:
-            print("paper")
+            c = "rock"
+            return c
+    else:
+        # User wins ["1"]
+        if u_in == "rock":
+            b = "scissors"
+            return b
+        elif u_in == "paper":
+            c = "rock"
+            return c
+        else:
+            a = "paper"
+            return a
+
+
+# def calc2(u_in, ai,gen_random):
+#     
+#     if gen_random == ["0"]:
+#         # Ai wins
+#         if 
         
-calc()
+def main():
+    
+    calc1(user_input(),gen_random())
+    
+print(main())
